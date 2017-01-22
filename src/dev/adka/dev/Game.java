ec216ef4cdabc22b528aa43b1757a10aa6ba7489
ADKA1987 <eng.alaa87@yahoo.com>
2017-01-22 12:14:31 +0100

    Threads && game loop

--- src/dev/adka/dev/Game.java
+++ src/dev/adka/dev/Game.java
@@ -2,13 +2,52 @@ package dev.adka.dev;
 
 import dev.adka.dev.display.Display;
 
-public class Game {
+public class Game implements Runnable{
 		private Display display;
 		public int width;
 		public int height;
+		public String title;
+		private boolean running;
+		private Thread thread;
 	public Game(String title, int width,int height){
 		this.width = width;
 		this.height = height;
-		display = new Display(title, width,height);
+		this.title = title;
 	}
+	private void init(){
+		display = new Display(title, width,height);	
+	}
+	private void tick(){
+		
+	}
+	private void render(){
+		
+	}
+	
+	public void run(){
+		init();	
+		while(running){
+			tick();
+			render();
+		}
+		stop();
+	}
+	public synchronized void start(){
+		if(running)
+			return;
+		running= true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
